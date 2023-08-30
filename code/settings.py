ae11fa40f480e57407163bc8f3776a6aeb8f2319
Scott Kjorlien <skjorlien@gmail.com>
2023-08-30 16:43:40 -0700

    settings update

--- code/settings.py
+++ code/settings.py
@@ -7,6 +7,7 @@ DATERANGE = range(2006, 2023)
 
 PARQUET_FOIA = os.path.join(os.getcwd(), 'data', 'clean', 'parquet-FOIA') 
 PARQUET_Public = os.path.join(os.getcwd(), 'data', 'clean', 'parquet-Public') 
+
 # When working from USB, Comment out above, and use below
 # PARQUET_FOIA = os.path.join('E:', 'data', 'Ethan-GSR','clean', 'parquet-FOIA') 
 # PARQUET_Public = os.path.join('E:', 'data', 'Ethan-GSR','clean', 'parquet-Public') 
